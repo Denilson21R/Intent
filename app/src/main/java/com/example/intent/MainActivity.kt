24a1d99f36fd00bec005ed86ff91d9ab7d9284e1
Denilson21R <45276744+Denilson21R@users.noreply.github.com>
2022-05-11 17:20:44 -0300

    incrementando parte de ligação e discagem

--- app/src/main/java/com/example/intent/MainActivity.kt
+++ app/src/main/java/com/example/intent/MainActivity.kt
@@ -1,23 +1,37 @@
 package com.example.intent
 
+import android.Manifest
+import android.Manifest.permission.CALL_PHONE
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.intent.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    companion object{
+    companion object {
         val PARAMETRO = "PARAMETRO"
         val RETORNO = "RETORNO"
         val OUTRA_ACTIVITY_REQUEST_CODE = 0
+        val RECEBER_RETORNAR_ACTION = "RECEBER_RETORNAR_ACTION"
     }
 
     private val activityMainBinding: ActivityMainBinding by lazy { //quando usa o lazy ele so instancia quando necessario
         ActivityMainBinding.inflate(layoutInflater)
     }
+
+    private lateinit var outraActivityResultLauncher: ActivityResultLauncher<Intent>
+
+    private lateinit var requisisaoPermissaoArl: ActivityResultLauncher<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +39,27 @@ class MainActivity : AppCompatActivity() {
 
         supportActionBar?.title = "Tratando Intents"
         supportActionBar?.subtitle = "Principais Tipos"
+
+        /*outraActivityResultLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult(),
+            object : ActivityResultCallback<ActivityResult> {
+                override fun onActivityResult(result:ActivityResult?){
+                if(result?.resultCode == RESULT_OK){
+                    val retorno = String = result.data?.getStringExtra(RETORNO)
+                    activityMainBinding.retornoTv.text = retorno
+                }
+            }
+        )*/
+
+        requisisaoPermissaoArl = registerForActivityResult(ActivityResultContracts.RequestPermission()){ permissaoConcedida ->
+            if (permissaoConcedida){
+                discarTelefone(Intent.ACTION_CALL)
+            }else{
+                //requisitarPermissaoLigacao()
+                Toast.makeText(this, "Permissão Necessária", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -36,7 +71,8 @@ class MainActivity : AppCompatActivity() {
         return when(item.itemId){
             R.id.outraActivityMi -> {
                 //declara intent
-                val outraActivityIntent: Intent = Intent(this, OutraActivity::class.java)
+                //val outraActivityIntent: Intent = Intent(this, OutraActivity::class.java)
+                val outraActivityIntent = Intent(RECEBER_RETORNAR_ACTION)
 
                 //criacao do bundle que vai para a proxima activity
                 val parametrosBundle = Bundle()
@@ -50,12 +86,38 @@ class MainActivity : AppCompatActivity() {
                 true
             }
             R.id.viewMi -> {
+                //url precisa conter http ou https
+                val url = activityMainBinding.parametroEt.text.toString().let {
+                    if(!it.lowercase().contains("http[s]?".toRegex())) "https://" + it else it
+                }
+                val siteIntent = Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse(url)
+                )
+                startActivity(siteIntent)
                 true
             }
             R.id.callMi -> {
+                //android >= M, verifica se tem permissao e solicita se necessario
+                //android < M, executa a discagem
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                    //android >= M
+                    if(checkSelfPermission(Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED){//verifica se tem permissao
+                        //tenho a permissao
+                        discarTelefone(Intent.ACTION_CALL)
+                    }else{
+                        //nao tenho a permissao
+                        requisitarPermissaoLigacao()
+                    }
+                }else{
+                    //android < M
+                    discarTelefone(Intent.ACTION_CALL)
+                }
+
                 true
             }
             R.id.dialMi -> {
+                discarTelefone(Intent.ACTION_DIAL)
                 true
             }
             R.id.pickMi -> {
@@ -74,5 +136,18 @@ class MainActivity : AppCompatActivity() {
             val retorno:String = data?.getStringExtra(RETORNO)?: ""
             activityMainBinding.retornoTv.text = retorno
         }
+    }
+
+    private fun discarTelefone(action: String){
+        val numero = activityMainBinding.parametroEt.text.toString()
+        val chamadaIntent = Intent(
+            action,
+            Uri.parse("tel:" + numero)
+        )
+        startActivity(chamadaIntent)
+    }
+
+    private fun requisitarPermissaoLigacao() {
+        requisisaoPermissaoArl.launch(CALL_PHONE)
     }
 }
